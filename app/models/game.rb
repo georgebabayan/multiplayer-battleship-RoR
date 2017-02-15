c578880ad3408e6ddc79c0ee8d85a44314840c6c
Jon <jon.bent@me.com>
2017-02-15 14:45:34 -0800

    adds associations for all classes, adds less hardcoded board

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,2 +1,7 @@
 class Game < ApplicationRecord
+	has_many :rounds
+	has_many :users, through: :rounds
+	belongs_to :winner, class_name: "User", foreign_key: "winner_id"
+	belongs_to :loser, class_name: "User", foreign_key: "loser_id"
+
 end
