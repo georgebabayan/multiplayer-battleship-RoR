bfa11df2187037a45487cecfc564479242a109c7
Amber Nash <nash.beram@gmail.com>
2017-02-15 17:50:08 -0800

    Initial build of sessions controller/helper.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      redirect_to @render
+      redirect_to @user
     else
       render 'new'
     end
