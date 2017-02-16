02201fe4fd428e178b5ae4c7d7141e2b29868d60
Jon <jon.bent@me.com>
2017-02-15 18:07:12 -0800

    fixes some database issues, and adds some logic to rounds model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170215224947) do
+ActiveRecord::Schema.define(version: 20170215235835) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20170215224947) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "game_id"
+    t.text     "board2"
     t.index ["game_id"], name: "index_rounds_on_game_id", using: :btree
     t.index ["user_id"], name: "index_rounds_on_user_id", using: :btree
   end
