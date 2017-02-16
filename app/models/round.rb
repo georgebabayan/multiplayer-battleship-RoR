5d48161957ac54df065f44a3b747cb1430925bb4
George Babayan <ggbabayan@gmail.com>
2017-02-15 20:55:03 -0800

    Add logic for user

--- app/models/round.rb
+++ app/models/round.rb
@@ -5,12 +5,7 @@ class Round < ApplicationRecord
   # b1 = self.board; b2 = self.board2
   # @board = [b1, b2]
 
-  def fit_checker(s_p, direction, length_of_ship, player)
-    if player == "player1"
-      board = @board[0]
-    elsif player == "player2"
-      board = @board[1]
-    end
+  def fit_checker(s_p, direction, length_of_ship, board)
 
     if direction == 'right'
       if (board[s_p[0]][(s_p[1])..length_of_ship]).include?("sheep")
@@ -25,24 +20,21 @@ class Round < ApplicationRecord
         return true
       end
     end
-
-
   end
+
 
   def sheep_placer(s_p, direction,board,ship_length)
     row=s_p[0].to_i; col=s_p[1].to_i
     if direction == "down" || direction == 0
       i= 0 ;
       while i < ship_length
-        board[row+i][col]="sheep"
-        i+=1
+        board[row+i][col]="sheep"; i+=1
       end
       return board
     elsif direction == "right" || direction == 1
       i= 0 ;
       while i < ship_length
-        board[row][i+col]="sheep"
-        i+=1
+        board[row][i+col]="sheep" ; i+=1
       end
 
       return board
@@ -60,8 +52,12 @@ class Round < ApplicationRecord
     end
   end
 
-  def finished?
-
+  def finished?(board)
+    count=0; total_number_of_sheep=31
+    board.each do |row|
+      count += row.count("sheep")
+    end
+    total_number_of_sheep == count
   end
 
   def winner?
