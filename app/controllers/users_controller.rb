be23fc0b09d63ac2508e188aaf01f1f3d7a165d8
Amber Nash <nash.beram@gmail.com>
2017-02-16 10:21:38 -0800

    Change param term, add password verify/validation feature to reg.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -39,7 +39,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:username, :password_digest)
+    params.require(:user).permit(:username, :password, :password_confirmation)
   end
 
   def find_user
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,8 @@ class User < ApplicationRecord
 	has_secure_password
 	has_many :games
 	has_many :rounds
-	
+
+	validates :username, presence: true
+	validates :username, uniqueness: true
 
 end
-
-
