990f8262541bcc23ac284d382dd2a8d673bf1112
George Babayan <ggbabayan@gmail.com>
2017-02-16 16:37:23 -0800

    Add logic for diplay sheep and Chat

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ApplicationRecord
 	has_secure_password
-	has_many :games
 	has_many :sides
+	has_many :games, through: :sides
 	has_many :messages
+	has_many :conversations, through: :messages
 
 	validates :username, presence: true
 	validates :username, uniqueness: true
